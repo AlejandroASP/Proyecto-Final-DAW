329d702fcded704ec238fde092029daab1657de6
alejandro <alejandroasp71@gmail.com>
2024-03-20 11:18:44 +0000

    prototipo de barra

--- Proyecto/src/App.jsx
+++ Proyecto/src/App.jsx
@@ -3,33 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
+    // Las <> Son el inicializador
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="mainbar">
+        <input type="search" class="search"/>
+        <img src="https://unavatar.io/kikobeats" alt="imagen usuario" class="user"/>
+        <div class="options">
+          <h1>Menu</h1>
+          <h1>WishList</h1>
+          <h1>Wallet</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
