1ac147791ef15005f8a876c9de527ebc39c37c12
Alejandro <alejandroasp71@gmail.com>
2024-05-08 15:56:10 +0100

    style(shop.jsx)
    Se añade el icono de la lupa a la vista de la tienda

--- Frontend/src/pages/shop.jsx
+++ Frontend/src/pages/shop.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import { SearchIcon } from '@heroicons/react/solid'; // Importa el icono de la lupa de Heroicons
 
 function Tienda() {
     const [busqueda, setBusqueda] = useState('');
@@ -10,13 +11,16 @@ function Tienda() {
         <>
             <Header />
             <div className="bg-violet-900 min-h-screen flex flex-col justify-center items-center">
-                <input
-                    type="text"
-                    placeholder="Buscar juegos"
-                    value={busqueda}
-                    onChange={e => setBusqueda(e.target.value)}
-                    className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full max-w-md"
-                />
+                <div className="relative w-full max-w-md">
+                    <input
+                        type="text"
+                        placeholder="Buscar juegos"
+                        value={busqueda}
+                        onChange={e => setBusqueda(e.target.value)}
+                        className="border border-gray-300 rounded-md px-3 py-2 pl-10 w-full"
+                    />
+                    <SearchIcon className="h-6 w-6 absolute top-2 left-3 text-gray-400" /> {/* Agrega el icono de la lupa */}
+                </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
                     {[...Array(6)].map((_, index) => (
                         <div key={index} className="bg-blue-200 p-4 rounded-md shadow-md relative overflow-hidden flex flex-col items-center">
