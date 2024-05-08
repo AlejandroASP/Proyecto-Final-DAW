b21f63ac0bb5af23982a2bd976f4590219ed8e23
Alejandro <alejandroasp71@gmail.com>
2024-05-08 13:44:59 +0100

    feat(shop.js index.jsx header.jsx app.jsx)
    Se modifican los diferentes archivos (modificación de rutas) y se crea la vista de la tienda

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,23 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from '../src/pages/login';
+import Login from './pages/login';
 import Index from './pages/index';
-import Register from '../src/pages/edit';
+import Register from './pages/register';
 import EditUser from '../src/pages/edit';
-
+import Shop from './pages/shop';
 
 export function App() {
   return (
     <div className="bg-zinc-100 sm:flex flex-col min-h-screen h-full">
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/' element={<Index />} />
-          <Route path='/register' element={<Register/>} />
-          <Route path='/edit' element={<EditUser/>}/>
+          <Route path='/register' element={<Register />} />
+          <Route path='/edit' element={<EditUser />} />
+          <Route path='/tienda' element={<Shop />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   )
 }
