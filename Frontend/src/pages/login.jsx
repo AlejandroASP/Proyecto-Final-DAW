44c1b98fb7ed199d324b6971760ebf36dd0e4195
AliothGH <guiller_1994@hotmail.com>
2024-05-10 17:59:01 +0100

    feat(proyecto): se cambia la forma de declarar usestate

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
