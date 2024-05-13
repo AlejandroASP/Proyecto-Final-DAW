41fee8f9902f1cc5adb5ec01125892b04be98a5a
Alejandro <alejandroasp71@gmail.com>
2024-05-13 12:42:20 +0100

    feat(shop.jsx)
    Se crea de nuevo el componente shop y se añade la query para mostrar los juegos de la base de datos(no funciona)

--- Frontend/src/pages/shop.jsx
+++ Frontend/src/pages/shop.jsx
@@ -1,34 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
-import { SearchIcon } from '@heroicons/react/solid'; // Importa el icono de la lupa de Heroicons
 
 function Tienda() {
+    const [productos, setProductos] = useState([]);
     const [busqueda, setBusqueda] = useState('');
+
+    useEffect(() => {
+        // Función para obtener los datos de los productos desde el servidor
+        const obtenerProductos = async () => {
+            try {
+                const response = await fetch('/tienda');
+                if (response.ok) {
+                    const data = await response.json();
+                    setProductos(data);
+                } else {
+                    console.error('Error al obtener los productos:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error al realizar la solicitud:', error);
+            }
+        };
+
+        obtenerProductos();
+    }, []); // La dependencia vacía asegura que esta función solo se ejecute una vez al montar el componente
 
     return (
         <>
             <Header />
             <div className="bg-violet-900 min-h-screen flex flex-col justify-center items-center">
-                <div className="relative w-full max-w-md">
-                    <input
-                        type="text"
-                        placeholder="Buscar juegos"
-                        value={busqueda}
-                        onChange={e => setBusqueda(e.target.value)}
-                        className="border border-gray-300 rounded-md px-3 py-2 pl-10 w-full"
-                    />
-                    <SearchIcon className="h-6 w-6 absolute top-2 left-3 text-gray-400" /> {/* Agrega el icono de la lupa */}
-                </div>
+                <input
+                    type="text"
+                    placeholder="Buscar productos"
+                    value={busqueda}
+                    onChange={e => setBusqueda(e.target.value)}
+                    className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full max-w-md"
+                />
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
-                    {[...Array(6)].map((_, index) => (
+                    {productos.map((producto, index) => (
                         <div key={index} className="bg-blue-200 p-4 rounded-md shadow-md relative overflow-hidden flex flex-col items-center">
-                            <img src="https://unavatar.io/kikobeats" alt="imagen producto" className="w-full h-auto mb-2" />
-                            <Link to="/details"
+                            <h3 className="text-lg font-bold mb-2">{producto.nombre}</h3>
+                            <p>Precio: ${producto.precio}</p>
+                            <Link to={`/detalles/${producto.id}`}
                                 className="transition-opacity duration-300 opacity-0 hover:opacity-100 
                                 absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50 hover:no-underline hover:text-white">
-                                Detalles del juego
+                                Detalles del producto
                             </Link>
                         </div>
                     ))}
