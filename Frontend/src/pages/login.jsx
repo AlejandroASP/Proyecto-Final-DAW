41166c9e3bd9b7c796c5fe3b6894057335600c57
AliothGH <guiller_1994@hotmail.com>
2024-05-16 10:52:02 +0100

    feat(login): se reimplementa poder iniciar sesión con un usuario de la base de datos y que se mantenga en el sessionStorage

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -2,18 +2,19 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
-import { EyeIcon, EyeOffIcon } from '@heroicons/react/solid'; // Importa los iconos de ojo y ojo desactivado de Heroicons
+import { EyeIcon, EyeOffIcon } from '@heroicons/react/solid';
 
 function Login() {
     const navigate = useNavigate();
-    const [showPassword, setShowPassword] = useState(false); // Estado para controlar si se muestra la contraseña o no
+    const [showPassword, setShowPassword] = useState(false);
+    const [error, setError] = useState(null);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const username = event.target.elements.username.value;
         const password = event.target.elements.password.value;
 
-        fetch('http://localhost:5173/login', {
+        fetch('http://localhost:3002/api/user/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -22,18 +23,21 @@ function Login() {
         })
             .then(response => response.json())
             .then(data => {
-                if (data.success) {
+                if (data.token) {
                     // Si el inicio de sesión es exitoso, redirige al usuario
+                    sessionStorage.setItem('token', data.token);
+                    sessionStorage.setItem('username', username);
+                    console.log('Token guardado en el sessionStorage:', data.token);
                     navigate('/');
                 } else {
                     // Si hay un error, muestra un mensaje de error
-                    console.error('Error:', data.message);
+                    setError(data.error);
                 }
             })
             .catch((error) => {
                 console.error('Error:', error);
+                setError('Hubo un error al iniciar sesión. Por favor, inténtalo de nuevo.');
             });
-
     };
 
     return (
@@ -43,6 +47,7 @@ function Login() {
                 <div className="min-h-screen flex justify-center items-center">
                     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full sm:max-w-md">
                         <h2 className="text-center text-3xl font-extrabold text-gray-900">Iniciar sesión</h2>
+                        {error && <div className="text-red-600 text-center mb-4">{error}</div>}
                         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                             <div className="space-y-4">
                                 <div>
@@ -52,7 +57,6 @@ function Login() {
                                 <div className="relative">
                                     <label htmlFor="password" className="sr-only">Contraseña</label>
                                     <input id="password" name="password" type={showPassword ? "text" : "password"} required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Contraseña" />
-                                    {/* Botón de ojo para mostrar/ocultar la contraseña */}
                                     <button
                                         type="button"
                                         className="absolute inset-y-0 right-0 flex items-center pr-3"
--- Frontend/src/services/user.js
+++ Frontend/src/services/user.js
@@ -32,3 +32,33 @@ export const registerUser = async (username, firstName, lastName, email, passwor
     console.log(error);
   }
 };
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${url}/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error de red: no se pudo conectar al servidor");
+    }
+
+    const responseJSON = await response.json();
+
+    if (!responseJSON.token) {
+      throw new Error(responseJSON.error || "Error desconocido al iniciar sesión");
+    }
+
+    return responseJSON.token;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
